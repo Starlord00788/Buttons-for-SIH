e80de7db3511edee11c217ecffdfffb6915174f7
Starlord00788 <palashsinghal000@gmail.com>
2024-09-06 20:08:30 +0530

    git add initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='box'><button className='button-56 '>
+     Lorem, ipsum.
+    </button>
+    <button className='button-60'>
+    Lorem, ipsum.
+    </button>
+
+    </div>
+    
+  );
+};
+
+export default App;
